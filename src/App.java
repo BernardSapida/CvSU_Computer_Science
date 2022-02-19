a772d12be49681adfa65fe410baec61c84fdd316
Bernard Sapida <BernardSapida1706@gmail.com>
2022-02-19 09:27:50 +0800

    init

---
+++ src/App.java
@@ -0,0 +1,43 @@
+public class App {
+    static int num = 5;
+    enum Numbers { a, b, c, d, e };
+    // static String[] names = {"Bernard", "Christina"};
+    public static void main(String[] args) throws Exception {
+        Numbers[] numbers = Numbers.values();
+        Color[] color = Color.values();
+
+        System.out.println(Color.RED.getColorAsInt());
+        Color.convertIntToColor(10);
+        for(Color colors: color){
+            System.out.println(colors);
+        }
+    }
+
+    public enum Color {
+        RED(10), 
+        BLUE(20), 
+        GREEN(30), 
+        YELLOW(40), 
+        BLACK(50);
+         
+        private final int color;
+        
+        Color(int color) {
+            this.color = color;
+        }
+
+        public int getColorAsInt() {
+            return color;
+        }
+        
+        public static Color convertIntToColor(int iColor) {
+            for (Color color : Color.values()) {
+                if (color.getColorAsInt() == iColor) {
+                    return color;
+                }
+            }
+            return null;
+        }
+     
+    }
+}
