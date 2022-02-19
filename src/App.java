1b52658790c5ccf606b0559a46b6b8007cf98119
Bernard Sapida <BernardSapida1706@gmail.com>
2022-02-19 10:41:27 +0800

    commit

--- src/App.java
+++ src/App.java
@@ -2,44 +2,34 @@
     Author: Bernard Sapida
 */
 public class App {
-    static int num = 5;
-    enum Numbers { a, b, c, d, e };
-    static String[] names = {"Bernard", "Sapida"};
     public static void main(String[] args) throws Exception {
-        // Numbers[] numbers = Numbers.values();
-        Color[] color = Color.values();
-        Color.convertIntToColor(10);
-        System.out.println("Available Colors: ");
-        for(Color colors: color){
-            System.out.println(Color.convertIntToColor(colors.getColorAsInt()) + ": " + colors.getColorAsInt());
+        Numbers[] numbers_name = Numbers.values();
+        Colors[] colors = Colors.values();
+        for(int i = 0; i < colors.length; i++){
+            System.out.println(colors[i].getColorAsInteger() + ": " + numbers_name[i] + " ("+colors[i]+")");
         }
     }
+    public static enum Numbers { One, Two, Three, Four, Five }
+    public static enum Colors { 
+        RED(1), BLUE(2), GREEN(3), VIOLET(4), SCARLET(5);
 
-    public enum Color {
-        RED(10), 
-        BLUE(20), 
-        GREEN(30), 
-        YELLOW(40), 
-        BLACK(50);
-         
-        private final int color;
-        
-        Color(int color) {
-            this.color = color;
+        private final int color_number;
+
+        Colors(int color_number){
+            this.color_number = color_number;
         }
 
-        public int getColorAsInt() {
-            return color;
+        public int getColorAsInteger(){
+            return color_number;
         }
-        
-        public static Color convertIntToColor(int iColor) {
-            for (Color color : Color.values()) {
-                if (color.getColorAsInt() == iColor) {
+
+        public static Colors getColorAsName(int number){
+            for(Colors color: Colors.values()){
+                if(number == color.getColorAsInteger()){
                     return color;
                 }
             }
             return null;
         }
-     
     }
 }
