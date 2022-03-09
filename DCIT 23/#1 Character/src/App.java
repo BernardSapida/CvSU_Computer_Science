0316e5ee2489eb589dd083bb3e21fb795a5eaf91
Bernard Sapida <BernardSapida1706@gmail.com>
2022-03-09 22:23:18 +0800

    push

--- DCIT 23/#1 Character/src/App.java
+++ DCIT 23/#1 Character/src/App.java
@@ -2,6 +2,41 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        System.out.println("███████  ███████  ██████   ███   ██  ███████ ██████  ██████  ");
+        System.out.println("██   ██  ██       ██   ██  ████  ██  ██   ██ ██   ██ ██   ██ ");
+        System.out.println("██████   █████    ██████   ██ ██ ██  ███████ ██████  ██   ██ ");
+        System.out.println("██   ██  ██       ██   ██  ██  ████  ██   ██ ██   ██ ██   ██ ");
+        System.out.println("███████  ███████  ██   ██  ██   ███  ██   ██ ██   ██ ██████  ");
+        System.out.println();
+        System.out.println("      ███████  ███████  ██████   ██  ██████    █████");
+        System.out.println("      ██       ██   ██  ██   ██  ██  ██   ██  ██   ██ ");
+        System.out.println("      ███████  ███████  ██████   ██  ██   ██  ███████ ");
+        System.out.println("           ██  ██   ██  ██       ██  ██   ██  ██   ██ ");
+        System.out.println("      ███████  ██   ██  ██       ██  ██████   ██   ██ ");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("                         ███     ███");
+        System.out.println("                       ███████ ███████");
+        System.out.println("                       ██   █████   ██");
+        System.out.println("                      ██ ██  ███ ██  ██");
+        System.out.println("                      ██ ██  ███ ██  ██");
+        System.out.println("                      ██     ███     ██");
+        System.out.println("                      ███   █████   ███");
+        System.out.println("                     ███████████████████");
+        System.out.println("                    ███████  ██  ████████");
+        System.out.println("                   ███████████████████████");
+        System.out.println("                  ██████             ██████");
+        System.out.println("                  ██                     ██");
+        System.out.println("                  █      ████████████     █");
+        System.out.println("                 ██       ██████████      ██");
+        System.out.println("                 ██        ████████        ██");
+        System.out.println("                ███         ██████         ███");
+        System.out.println("               █████         ████        █████");
+        System.out.println("               █████          ██         █████");
+        System.out.println("                █████                   █████");
+        System.out.println("                 ██████               ███████");
+        System.out.println("                ██████████████████████████████");
+        System.out.println("               ███ ███                 ███ ████");
+        System.out.println("                   ███                      ███");
     }
 }
