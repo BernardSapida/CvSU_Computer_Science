6e29cd0baef29a5147a55a945b902a4fd6dea8a6
Bernard Sapida <BernardSapida1706@gmail.com>
2022-03-10 18:48:06 +0800

    push

--- DCIT 23/Laboratory #1 - Character/src/App.java
+++ DCIT 23/Laboratory #1 - Character/src/App.java
@@ -2,19 +2,19 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("\u001B[36m");
         System.out.println("███████  ███████  ██████   ███   ██  ███████ ██████  ██████  ");
         System.out.println("██   ██  ██       ██   ██  ████  ██  ██   ██ ██   ██ ██   ██ ");
         System.out.println("██████   █████    ██████   ██ ██ ██  ███████ ██████  ██   ██ ");
         System.out.println("██   ██  ██       ██   ██  ██  ████  ██   ██ ██   ██ ██   ██ ");
         System.out.println("███████  ███████  ██   ██  ██   ███  ██   ██ ██   ██ ██████  ");
-        System.out.println();
+        System.out.println("\u001B[31m");
         System.out.println("      ███████  ███████  ██████   ██  ██████    █████");
         System.out.println("      ██       ██   ██  ██   ██  ██  ██   ██  ██   ██ ");
         System.out.println("      ███████  ███████  ██████   ██  ██   ██  ███████ ");
         System.out.println("           ██  ██   ██  ██       ██  ██   ██  ██   ██ ");
         System.out.println("      ███████  ██   ██  ██       ██  ██████   ██   ██ ");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("\u001B[34m");
         System.out.println("                         ███     ███");
         System.out.println("                       ███████ ███████");
         System.out.println("                       ██   █████   ██");
