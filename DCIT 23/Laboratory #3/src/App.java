2d17667dc59fc175c7a99cbc98adf6352104a4f6
Bernard Sapida <BernardSapida1706@gmail.com>
2022-03-30 11:19:12 +0800

    push

--- DCIT 23/Laboratory #3/src/App.java
+++ DCIT 23/Laboratory #3/src/App.java
@@ -1,5 +1,6 @@
 public class App {
+    private String greet = "Hello";
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
