868b80a1438562a89af87b82eeae743f2474da1e
BernardSapida <bernardsapida1706@gmail.com>
2022-05-02 18:54:46 +0800

    commit

--- ITEC 50B/Finals/js/header.js
+++ ITEC 50B/Finals/js/header.js
@@ -1,3 +1,8 @@
+window.onload = function() { 
+  const loader = document.querySelector('.loader_burgerhub');
+  loader.remove();
+};
+
 $(document).ready(function(){
     $("#navigation_responsive").click(function(){
       $("ul.nav_links").toggleClass("open dark", $('#hamburger-menu')[0].checked);
