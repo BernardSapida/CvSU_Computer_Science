4a3060f53870103a87e64091e95f0ee3de8cb180
BernardSapida <bernardsapida1706@gmail.com>
2022-05-05 20:44:07 +0800

    commit

---
+++ ITEC 50B/Finals/js/menu-acc.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#featured-product-1-minus-btn, #featured-product-2-minus-btn, #featured-product-3-minus-btn,\
+    #burger-1-minus-btn, #burger-2-minus-btn, #burger-3-minus-btn, #burger-4-minus-btn, #burger-5-minus-btn, #burger-6-minus-btn,\
+    #pizza-1-minus-btn, #pizza-2-minus-btn, #pizza-3-minus-btn, #pizza-4-minus-btn, #pizza-5-minus-btn, #pizza-6-minus-btn,\
+    #fries-1-minus-btn, #fries-2-minus-btn, #fries-3-minus-btn, #fries-4-minus-btn, #fries-5-minus-btn, #fries-6-minus-btn,\
+    #drinks-1-minus-btn, #drinks-2-minus-btn, #drinks-3-minus-btn, #drinks-4-minus-btn, #drinks-5-minus-btn, #drinks-6-minus-btn").click(function(){
+        $(`#${$(this).attr('id').slice(0, -10)}`).val(Number($(`#${$(this).attr('id').slice(0, -10)}`).val())-1);
+    });
+
+    $("#featured-product-1-plus-btn, #featured-product-2-plus-btn, #featured-product-3-plus-btn,\
+    #burger-1-plus-btn, #burger-2-plus-btn, #burger-3-plus-btn, #burger-4-plus-btn, #burger-5-plus-btn, #burger-6-plus-btn,\
+    #pizza-1-plus-btn, #pizza-2-plus-btn, #pizza-3-plus-btn, #pizza-4-plus-btn, #pizza-5-plus-btn, #pizza-6-plus-btn,\
+    #fries-1-plus-btn, #fries-2-plus-btn, #fries-3-plus-btn, #fries-4-plus-btn, #fries-5-plus-btn, #fries-6-plus-btn,\
+    #drinks-1-plus-btn, #drinks-2-plus-btn, #drinks-3-plus-btn, #drinks-4-plus-btn, #drinks-5-plus-btn, #drinks-6-plus-btn").click(function(){
+        $(`#${$(this).attr('id').slice(0, -9)}`).val(Number($(`#${$(this).attr('id').slice(0, -9)}`).val())+1);
+    });
+
+    $("#btn_cart-featured-product-1, #btn_cart-featured-product-2, #btn_cart-featured-product-3,\
+    #btn_cart-burger-1, #btn_cart-burger-2, #btn_cart-burger-3, #btn_cart-burger-4, #btn_cart-burger-5, #btn_cart-burger-6,\
+    #btn_cart-pizza-1, #btn_cart-pizza-2, #btn_cart-pizza-3, #btn_cart-pizza-4, #btn_cart-pizza-5, #btn_cart-pizza-6,\
+    #btn_cart-fries-1, #btn_cart-fries-2, #btn_cart-fries-3, #btn_cart-fries-4, #btn_cart-fries-5, #btn_cart-fries-6,\
+    #btn_cart-drinks-1, #btn_cart-drinks-2, #btn_cart-drinks-3, #btn_cart-drinks-4, #btn_cart-drinks-5, #btn_cart-drinks-6").click(function(e){
+        e.preventDefault();
+        console.log($(`#${$(this).attr('id').slice(9,)}`).val());
+    });
+});
+  
