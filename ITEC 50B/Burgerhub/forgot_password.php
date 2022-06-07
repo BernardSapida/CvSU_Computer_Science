dafdb70c56c2a175ac8381affaef196b17a88302
BernardSapida <bernardsapida1706@gmail.com>
2022-06-08 03:14:36 +0800

    commit

--- ITEC 50B/Burgerhub/forgot_password.php
+++ ITEC 50B/Burgerhub/forgot_password.php
@@ -5,13 +5,13 @@
     if($_SESSION["type"] == "client") header("Location: index.php");
     if($_SESSION["type"] == "admin") header("Location: index.php");
 
-    // use PHPMailer\PHPMailer\PHPMailer;
-    // use PHPMailer\PHPMailer\SMTP;
-    // use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
 
-    // require 'PHPMailer/src/Exception.php';
-    // require 'PHPMailer/src/PHPMailer.php';
-    // require 'PHPMailer/src/SMTP.php';
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
 
     $success_email = false;
 
@@ -30,53 +30,53 @@
                     $_SESSION["token_email"] = $email;
                     $_SESSION["token_url"] = uniqid();
 
-                    // $mail = new PHPMailer(true);
+                    $mail = new PHPMailer(true);
 
                     try {
                         // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                                   //Display output
-                        // $mail->isSMTP(); 
-                        // $mail->Host       = 'smtp.office365.com';                                   //Set the SMTP server to send through
-                        // $mail->SMTPAuth   = true;                                                   //Enable SMTP authentication
-                        // $mail->Username   = 'burgerhub.service@outlook.com';                        //SMTP username
-                        // $mail->Password   = 'burgerhub30241715';                                    //SMTP password
-                        // $mail->SMTPSecure = "STARTTLS";                                             //Enable implicit TLS encryption
-                        // $mail->Port       = 587;                                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                        $mail->isSMTP(); 
+                        $mail->Host       = 'smtp.office365.com';                                   //Set the SMTP server to send through
+                        $mail->SMTPAuth   = true;                                                   //Enable SMTP authentication
+                        $mail->Username   = 'burgerhub.service@outlook.com';                        //SMTP username
+                        $mail->Password   = 'burgerhub30241715';                                    //SMTP password
+                        $mail->SMTPSecure = "STARTTLS";                                             //Enable implicit TLS encryption
+                        $mail->Port       = 587;                                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         
                         //Recipients
-                        // $mail->setFrom('burgerhub.service@outlook.com', 'BurgerHub Service');
-                        // $mail->addAddress($email, $row_result['fullname']);                                           //Add a recipient & Name is optional
-                        // $mail->addReplyTo('burgerhub.service@outlook.com', 'Reply');
+                        $mail->setFrom('burgerhub.service@outlook.com', 'BurgerHub Service');
+                        $mail->addAddress($email, $row_result['fullname']);                         //Add a recipient & Name is optional
+                        $mail->addReplyTo('burgerhub.service@outlook.com', 'Reply');
         
                         //Content
-                        // $mail->isHTML(true);                                                        //Set email format to HTML
-                        // $mail->Subject = 'Message from BurgerHub';
-                        // $mail->Body    = 'We received your message, thank you for reaching us out and we will try out best to respond soonest!<br/>- Mr. Bernard';
-                        // $mail->Body    = '  <section class="section_email-template" style="padding: 15px 30px; position: relative; background-color: black;">
-                        //                         <div class="container_email_header" style="margin-bottom: 10px;">
-                        //                             <p class="email_header-logo"><img src="https://i.ibb.co/dcBt23r/279377135-1117847642111961-6949520978733401421-n.png" alt="BurgerHub Logo" style="display: block; margin: auto; width: 100%;  max-width: 120px;"></p>
-                        //                         </div>
-                        //                         <div class="container_email-body" style="margin: auto; background-color: rgb(255, 247, 232); border-radius: 5px; display: grid; place-items: center; padding: 0 50px;">
-                        //                             <div class="container_email-body-content" style="padding:20px 0 30px 0;">
-                        //                                 <h1 style="text-align: center; line-height: 1; margin-bottom: 8px; font-size: 34px; color: hsl(0, 0%, 2%);">Hello, <span style="color: hsl(349, 100%, 54%);">' . $row_result['fullname'] . '</span></h1>
-                        //                                 <p style="text-align: center; line-height: 1; margin-bottom: 4px;font-weight: 700; color: hsl(0, 0%, 2%);">We received your reset password request.</p>
-                        //                                 <p style="text-align: center; line-height: 1; color: hsl(0, 0%, 2%);">Here\'s the link to reset your password <a href="http://localhost/finals31/reset_password.php?token=' . $_SESSION["token_url"] . '">click link</a></p>
-                        //                             </div>
-                        //                        </div>
-                        //                         <div class="container_email-footer" style="margin: 15px 0; position: relative; z-index: 1; color: hsl(0, 0%, 100%);">
-                        //                             <ul style="text-align: center;">
-                        //                                 <li style="display: inline-block; list-style-type: none;"><a href="" style="padding: 0 8px; text-decoration: none; color: hsl(47, 100%, 44%); font-weight: 600; font-size: 14px;">Home</a> |</li>
-                        //                                 <li style="display: inline-block; list-style-type: none;"><a href="" style="padding: 0 8px; text-decoration: none; color: hsl(47, 100%, 44%); font-weight: 600; font-size: 14px;">About</a> |</li>
-                        //                                 <li style="display: inline-block; list-style-type: none;"><a href="" style="padding: 0 8px; text-decoration: none; color: hsl(47, 100%, 44%); font-weight: 600; font-size: 14px;">Contact</a> |</li>
-                        //                                 <li style="display: inline-block; list-style-type: none;"><a href="" style="padding: 0 8px; text-decoration: none; color: hsl(47, 100%, 44%); font-weight: 600; font-size: 14px;">Menu</a> |</li>
-                        //                                 <li style="display: inline-block; list-style-type: none;"><a href="" style="padding: 0 8px; text-decoration: none; color: hsl(47, 100%, 44%); font-weight: 600; font-size: 14px;">Sign In</a></li>
-                        //                             </ul>
-                        //                             <p class="email-reminder" style="text-align: center; margin: 10px; font-size: 12px; color: hsl(0, 0%, 100%, .5); font-weight: 300;">This is an automated email. Please do not reply.</p>
-                        //                             <p class="email-copyright" style="text-align: center; margin: 10px; font-size: 12px; color: hsl(0, 0%, 100%, .5); font-weight: 300;">Copyright &copy;2022 BurgerHub. All Rights Reserved.</p>
-                        //                         </div>
-                        //                     </section>';
-                        // $mail->AltBody = 'We received your message, thank you for reaching us out and we will try out best to respond soonest!';
+                        $mail->isHTML(true);                                                        //Set email format to HTML
+                        $mail->Subject = 'Message from BurgerHub';
+                        $mail->Body    = 'We received your message, thank you for reaching us out and we will try out best to respond soonest!<br/>- Mr. Bernard';
+                        $mail->Body    = '  <section class="section_email-template" style="padding: 15px 30px; position: relative; background-color: black;">
+                                                <div class="container_email_header" style="margin-bottom: 10px;">
+                                                    <p class="email_header-logo"><img src="https://i.ibb.co/dcBt23r/279377135-1117847642111961-6949520978733401421-n.png" alt="BurgerHub Logo" style="display: block; margin: auto; width: 100%;  max-width: 120px;"></p>
+                                                </div>
+                                                <div class="container_email-body" style="margin: auto; background-color: rgb(255, 247, 232); border-radius: 5px; display: grid; place-items: center; padding: 0 50px;">
+                                                    <div class="container_email-body-content" style="padding:20px 0 30px 0;">
+                                                        <h1 style="text-align: center; line-height: 1; margin-bottom: 8px; font-size: 34px; color: hsl(0, 0%, 2%);">Hello, <span style="color: hsl(349, 100%, 54%);">' . $row_result['fullname'] . '</span></h1>
+                                                        <p style="text-align: center; line-height: 1; margin-bottom: 4px;font-weight: 700; color: hsl(0, 0%, 2%);">We received your reset password request.</p>
+                                                        <p style="text-align: center; line-height: 1; color: hsl(0, 0%, 2%);">Here\'s the link to reset your password <a href="http://localhost/finals31/reset_password.php?token=' . $_SESSION["token_url"] . '">click link</a></p>
+                                                    </div>
+                                               </div>
+                                                <div class="container_email-footer" style="margin: 15px 0; position: relative; z-index: 1; color: hsl(0, 0%, 100%);">
+                                                    <ul style="text-align: center;">
+                                                        <li style="display: inline-block; list-style-type: none;"><a href="" style="padding: 0 8px; text-decoration: none; color: hsl(47, 100%, 44%); font-weight: 600; font-size: 14px;">Home</a> |</li>
+                                                        <li style="display: inline-block; list-style-type: none;"><a href="" style="padding: 0 8px; text-decoration: none; color: hsl(47, 100%, 44%); font-weight: 600; font-size: 14px;">About</a> |</li>
+                                                        <li style="display: inline-block; list-style-type: none;"><a href="" style="padding: 0 8px; text-decoration: none; color: hsl(47, 100%, 44%); font-weight: 600; font-size: 14px;">Contact</a> |</li>
+                                                        <li style="display: inline-block; list-style-type: none;"><a href="" style="padding: 0 8px; text-decoration: none; color: hsl(47, 100%, 44%); font-weight: 600; font-size: 14px;">Menu</a> |</li>
+                                                        <li style="display: inline-block; list-style-type: none;"><a href="" style="padding: 0 8px; text-decoration: none; color: hsl(47, 100%, 44%); font-weight: 600; font-size: 14px;">Sign In</a></li>
+                                                    </ul>
+                                                    <p class="email-reminder" style="text-align: center; margin: 10px; font-size: 12px; color: hsl(0, 0%, 100%, .5); font-weight: 300;">This is an automated email. Please do not reply.</p>
+                                                    <p class="email-copyright" style="text-align: center; margin: 10px; font-size: 12px; color: hsl(0, 0%, 100%, .5); font-weight: 300;">Copyright &copy;2022 BurgerHub. All Rights Reserved.</p>
+                                                </div>
+                                            </section>';
+                        $mail->AltBody = 'We received your message, thank you for reaching us out and we will try out best to respond soonest!';
         
-                        // $mail->send();
+                        $mail->send();
                         
                         $success_email = true;
                         $email = "";
